43f054d0c22c39092ceccecccb35955d37cd0e57
Kedfy Gonzalez <kedfygo@gmail.com>
2019-12-09 15:35:51 -0600

    before autocomplete

--- payments/models.py
+++ payments/models.py
@@ -9,13 +9,13 @@ class Payments(models.Model):
     #amount = models.IntegerField(verbose_name="monto del pago")
     amount = models.DecimalField(decimal_places=2, max_digits=20, verbose_name="monto", blank=False, null=False, )
     type_of_payment = models.CharField(max_length=50, verbose_name="tipo de pago")
-    proof_of_payment = models.ImageField(upload_to='payments', verbose_name="Comprobante de Pago")
-    date_of_payment = models.DateTimeField(verbose_name="fecha de pago")
+    proof_of_payment = models.ImageField(upload_to='payments/', verbose_name="Comprobante de Pago",)
+    date_of_payment = models.DateTimeField( verbose_name="fecha de pago")
 
     class Meta:
         verbose_name = "Pago"
         verbose_name_plural = "Pagos"
         ordering = ['-date_of_payment']
-     
+    
     def __str__(self):
         return (self.name + ' ' + self.lastname)
--- residents/models.py
+++ residents/models.py
@@ -3,15 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Residents(models.Model):
-    TYPE_OF_PROPERTY_CHOICES = (('1', 'Casa'), ('2', 'Departamento'), ('3', 'Lote Baldío'))
-
     name = models.CharField(max_length=50, verbose_name="nombre")
     lastname = models.CharField(max_length=50, verbose_name="apellido")
     address = models.CharField(max_length=100, verbose_name="dirección")
     phone_number = models.CharField(max_length=10, verbose_name="número telefónico", blank=True)
     email = models.EmailField(verbose_name="correo electrónico")
-    #type_of_property = models.CharField(max_length=50, verbose_name="tipo de inmueble")
-    type_of_property = models.CharField(max_length=1, choices=TYPE_OF_PROPERTY_CHOICES)
+    type_of_property = models.CharField(max_length=50, verbose_name="tipo de inmueble")
     owner_name = models.CharField(max_length=100, verbose_name="nombre del propietario")
     owner_phone_number = models.CharField(max_length=10, verbose_name="número telefónico del propietario", blank=True)
 
